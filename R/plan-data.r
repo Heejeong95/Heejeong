88c4227fe31693274c4748e9a4d585de618b257f
Heejeong95 <ggaymon@naver.com>
2020-11-29 11:33:38 +0900

    1

---
+++ R/plan-data.r
@@ -0,0 +1,14 @@
+#' Plan data
+#'
+#' Data from 'data.Plan.go.kr'
+#'
+#' @docType data
+#'
+#' @usage data(Plan)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#'
+#' ## Requires font for korean text
+#' data(Plan)
